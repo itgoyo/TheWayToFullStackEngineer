289fbda16d494563d355f89aa9162d967c78701c
itgoyo <itgoyo@foxmail.com>
2019-08-16 08:21:46 +0800

    add vue-cli2 demo

---
+++ xmgit/Vue/01-LearnVueRouter/src/components/About.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div>
+    <h2>我是关于</h2>
+    <p>我是关于，内容</p>
+  </div>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name: "About"
+};
+</script>
+
+<style scoped>
+</style>
+
+
+
+
+
---
+++ xmgit/Vue/01-LearnVueRouter/src/components/Home.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div>
+    <h2>我是首页</h2>
+    <p>我是首页，内容</p>
+  </div>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped>
+</style>
+
+
+
+
+
